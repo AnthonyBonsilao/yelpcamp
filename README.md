6fa2e516d5607b9ebbe557f022e9c4d4031cebd5
Cheese <bonsilaoanthony@gmail.com>
2022-02-14 08:57:05 -0500

    read.md and js changes

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # yelpcamp
+Past in Git for Dependencies:
+    node i ejs ejs-mate express method-override mongoose morgan
--- models/campground.js
+++ models/campground.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const CampgroundSchema = new mongoose.Schema({
     title: String,
     price: Number,
-    image: 'String',
+    image: String,
     description: String,
     location: String
 });
